01487e9ca2990ad66e30ec9c2c87a368deb2513c
Matti Luukkainen <mluukkai@iki.fi>
2014-03-05 23:32:15 +0200

    kongo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, express, port;
+  var app, dburl, express, port;
 
   express = require('express');
 
@@ -12,11 +12,92 @@
     return app.set('view engine', 'ejs');
   });
 
+  global.mongoose = require('mongoose');
+
+  global.Schema = mongoose.Schema;
+
+  global.ObjectId = Schema.ObjectId;
+
+  dburl = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017/mydb";
+
+  mongoose.connect(dburl);
+
   port = "4000";
 
   app.listen(process.env.PORT || port);
 
   console.log("Server Started at http://localhost:" + port);
+
+  global.Todo = mongoose.model('Todo', new Schema({
+    id: ObjectId,
+    title: {
+      type: String
+    },
+    state: {
+      type: String
+    },
+    created_at: {
+      type: Date,
+      "default": Date.now
+    }
+  }));
+
+  app.get('/mongo', function(req, res) {
+    return res.send("Mongo word!");
+  });
+
+  app.get('/mongot/:id', function(req, res) {
+    var _this = this;
+    return Todo.findById(req.param('id'), function(err, todo) {
+      _this.todo = todo;
+      if (err != null) {
+        return res.send("no found " + (req.param('id')));
+      } else {
+        return res.json(_this.todo);
+      }
+    });
+  });
+
+  app["delete"]('/mongot/:id', function(req, res) {
+    var _this = this;
+    return Todo.findById(req.param('id'), function(err, todo) {
+      _this.todo = todo;
+      if (err != null) {
+        return res.send("no found " + (req.param('id')));
+      } else {
+        _this.todo.remove();
+        return res.json(_this.todo);
+      }
+    });
+  });
+
+  app.post('/mongot', function(req, res) {
+    var todo,
+      _this = this;
+    todo = new Todo({
+      title: req.param('title'),
+      state: req.param('state')
+    });
+    return todo.save(function(err) {
+      if (err != null) {
+        return res.json({});
+      } else {
+        return res.json(todo);
+      }
+    });
+  });
+
+  app.get('/mongot', function(req, res) {
+    var _this = this;
+    return Todo.find({}, function(err, todos) {
+      _this.todos = todos;
+      if (err != null) {
+        return res.send("bad");
+      } else {
+        return res.json(_this.todos);
+      }
+    });
+  });
 
 }).call(this);
 
--- src/coffee/node/config.coffee
+++ src/coffee/node/config.coffee
@@ -6,6 +6,59 @@ app.configure ->
 	app.set('views', "#{__dirname}/app/views")
 	app.set('view engine', 'ejs')
 
+global.mongoose = require('mongoose')
+global.Schema = mongoose.Schema
+global.ObjectId = Schema.ObjectId
+dburl = process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL || "mongodb://localhost:27017/mydb"
+mongoose.connect(dburl)
+
 port = "4000"
 app.listen(process.env.PORT || port);
 console.log "Server Started at http://localhost:#{port}"	
+
+global.Todo = mongoose.model 'Todo', new Schema
+	id: ObjectId
+	title:
+		type: String
+	state:
+		type: String
+	created_at:
+		type: Date
+		default: Date.now
+
+
+app.get '/mongo', (req,res) ->
+	res.send "Mongo word!"
+
+app.get '/mongot/:id', (req,res) ->
+	Todo.findById req.param('id'), (err, @todo) =>
+		if err?
+			res.send "no found #{req.param('id')}"
+		else
+			res.json @todo
+
+app.delete '/mongot/:id', (req,res) ->
+	Todo.findById req.param('id'), (err, @todo) =>
+		if err?
+			res.send "no found #{req.param('id')}"
+		else
+			@todo.remove()
+			res.json @todo
+
+app.post '/mongot', (req,res) ->
+
+	todo = new Todo({title: req.param('title'), state: req.param('state')})
+	todo.save (err) =>
+		if err?
+			res.json {}
+		else
+			res.json todo
+
+app.get '/mongot', (req,res) ->
+	Todo.find {}, (err, @todos) =>
+		if err?
+			res.send "bad"
+		else
+			res.json @todos
+
